41a93cadf3eaa7a3686d170726da8011068022a0
Endi <endre1988@freemail.hu>
2023-09-08 13:04:56 +0200

    függvények, ciklusok

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 1. feladat
 const nums = [3, 4, 9, 6, 2];
 const numbers = [];
 let divide = function (nums) {
@@ -12,6 +13,7 @@ let divide = function (nums) {
 divide(nums);
 document.querySelector(".nums").textContent = numbers;
 
+// 2. feladat
 const players = ["Peter", "Kate", "John"];
 const playersout = [];
 
@@ -23,6 +25,7 @@ let writer = function (players) {
 writer(players);
 document.querySelector(".players").textContent = playersout;
 
+// 3. feladat
 const x = ["", 4, true];
 const types = [];
 let check = function (x) {
@@ -34,3 +37,46 @@ let check = function (x) {
 check(x);
 
 document.querySelector(".types").textContent = types;
+
+// 4. feladat
+const names = [
+  "István",
+  "Katalin",
+  "Attila",
+  "Gábor",
+  "Péter",
+  "Anna",
+  "Kitti",
+];
+let valogato = function (nums) {
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] == "Gábor") {
+      document.querySelector(".names").textContent = `A tömb ${[
+        i + 1,
+      ]}. eleme ${names[i]}`;
+    }
+  }
+};
+valogato(names);
+
+// 5. feladat
+
+const numbs = ["a", "4", 2, 2, 1, 1]; // 10
+
+let adder = function (numbs) {
+  let sum = 0;
+  for (let i = 0; i < numbs.length; i++) {
+    if (!isNaN(numbs[i])) {
+      sum = sum + parseInt(numbs[i]);
+      document.querySelector(
+        ".numbs"
+      ).textContent = `A tömb értékeinek összege: ${[sum]}`;
+    } else {
+      console.log(
+        "A tömb legalább egy eleme nem szám, tehát nem konvertálható"
+      );
+    }
+  }
+};
+
+adder(numbs);
